8b6795cf3cec9d9a3d37cc7a96d139ac5feed569
Daniel-deborgher <64898999+Daniel-deborgher@users.noreply.github.com>
2024-07-29 14:07:55 +0200

    Update telegram_forwarder.py

--- telegram_forwarder.py
+++ telegram_forwarder.py
@@ -1,24 +1,15 @@
 from telethon import TelegramClient, events
-from telethon.sessions import StringSession
-import logging
-
-# Configuration du logging
-logging.basicConfig(level=logging.DEBUG)
 
 # Remplacez par votre API ID et API hash
 api_id = '26210305'
 api_hash = '34cbab68b08112262fa311a7affdd9d6'
 
-# Identifiant du groupe et du canal (utilisez les ID numériques si possible)
-group_id = 'testreplysol'
-channel_id = 'solana_trojanbot'
+# Identifiant du groupe et du canal
+group_id = 'testreplysol'  # Remplacez par l'identifiant ou le nom d'utilisateur de votre groupe
+channel_id = 'solana_trojanbot'  # Remplacez par le nom d'utilisateur de votre canal
 
-# Utiliser une session temporaire en mémoire
-session = StringSession()
-
-client = TelegramClient(session, api_id, api_hash, connection_retries=None, timeout=30)
-
-print("Client created")
+# Créer une nouvelle session Telethon
+client = TelegramClient('session_name', api_id, api_hash)
 
 @client.on(events.NewMessage(chats=group_id))
 async def handler(event):
@@ -28,14 +19,10 @@ async def handler(event):
     print(f"Message forwarded to {channel_id}")
 
 async def main():
-    print("Starting client...")
     await client.start()
     print("Client started. Listening for messages...")
     await client.run_until_disconnected()
-    print("Client disconnected")
 
 # Démarrer le client
 with client:
-    print("Running the client")
-    client.loop.run_until_complete(main())
-    print("Client has been run")
+    client.loop.run_until_complete(main()
